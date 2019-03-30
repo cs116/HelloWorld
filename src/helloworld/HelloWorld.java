c52c80d70dfe71621b3bcb0cac0c4a4c88e5165d
George <George@George-THINK>
2019-03-30 07:06:49 -0400

    initial commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,23 @@
+/*
+ * File:        HelloWorld.java
+ * Author:      George Corser (george@corser.com)
+ * Date:        March 30, 2019
+ * 
+ * Description: This program prints "Hello, world"
+ */
+package helloworld;
+
+/**
+ *
+ * @author George
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+    
+}
